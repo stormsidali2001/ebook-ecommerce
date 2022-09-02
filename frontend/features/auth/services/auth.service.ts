1ad95c174daa8d1138eb3961cf206cc0122bc4e3
stormsidali2001 <assoulsidali@gmail.com>
2022-09-02 15:32:07 -0700

    ...

--- frontend/features/auth/services/auth.service.ts
+++ frontend/features/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { DisplayUser } from "../models/DisplayUser.interface";
+import { LoginUser } from "../models/LoginUser.interface";
 import { NewUser } from "../models/NewUser";
 
 const register = async (newUser:NewUser):Promise<DisplayUser | null>=>{
@@ -7,7 +8,13 @@ const register = async (newUser:NewUser):Promise<DisplayUser | null>=>{
     const response = await axios.post(`http://localhost:8080/api/auth/register`, newUser)
     return response.data;
 }
+
+const login = async (user:LoginUser):Promise<DisplayUser | null>=>{
+    const response = await axios.post(`http://localhost:8080/api/auth/login`, user)
+    return response.data;
+}
 const authService = {
     register,
+    login
 }
 export default authService;
